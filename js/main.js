cc2d45a5666255e31dd337eff846f75315da07ca
CJ Faulk <cjfaulk@ufl.edu>
2020-10-02 14:22:41 -0400

    Add pizza object to main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Part C & D
+
+var pizza = {
+  crust: "thin",
+  cheese: "mozzarella",
+  sauce: "tomato",
+  addToppings: function() {
+    return this.console.log("Please add pepperoni and sausage");
+  }
+};
